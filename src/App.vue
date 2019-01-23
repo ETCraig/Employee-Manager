99af44faaf5d7b0ccd6678b964197dac6df91657
unknown <devethancraig@gmail.com>
2019-01-23 12:55:39 -0700

    Cleaned Up the Code

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Navbar />
+    <Navbar/>
     <div class="container">
       <router-view/>
     </div>
@@ -8,12 +8,12 @@
 </template>
 
 <script>
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Navbar
   }
-}
+};
 </script>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,14 +1,9 @@
 <template>
-    <nav>
-        <div class="nav-wrapper green">
-            <div class="container">
-                <router-link 
-                    to="/"
-                    class="brand-logo"
-                >
-                Employee Manager
-                </router-link>
-            </div>
-        </div>
-    </nav>
+  <nav>
+    <div class="nav-wrapper green">
+      <div class="container">
+        <router-link to="/" class="brand-logo">Employee Manager</router-link>
+      </div>
+    </div>
+  </nav>
 </template>
