c189fb8d27ef8f93fc3e21774878349ce79db881
unknown <devethancraig@gmail.com>
2019-02-06 12:20:20 -0700

    Attempting to make Live Site Routing Work

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,7 @@ Vue.use(Router)
 let router = new Router({
   routes: [
     {
-      path: '/',
+      path: '/Employee-Manager/',
       name: 'dashboard',
       component: Dashboard,
       meta: {
